86571a6dfe66b7e4ae5b364408daeaa1f0e9f484
Felipe Gomez <fl.gomez10@uniandes.edu.co>
2014-09-11 17:18:36 -0500

    Parallelization. Major changes. Results with and without extinction

---
+++ program_trieste/constants.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pylab as P
+
+hpl = 0.6777
+Mag0 = -4.61455/0.2587
+
+# Number of Monte-Carlo Steps
+MS = 10000
+
+# Monte-Carlo tuning
+DeltaChi = 0.0
+K0 = 0.01        # L_0
+K1 = 0.01        # M_0
+K2 = 0.05        # beta 
+K3 = 0.05        # gamma
+
+# Dust Attenuation/Extinction
+# only if Dust_Ext == 1
+Dust_Ext = 0
+
